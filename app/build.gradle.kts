427defca2b06c26cc7a0e6e35416dea0da185ace
msait <msaitoappdev@gmail.com>
2026-05-18 21:18:21 +0900

    アプリ起動時にリマインダー登録を行うように修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.msaitodev.passenger.opm.workpro"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.1"
+        versionCode = 5
+        versionName = "1.0.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
